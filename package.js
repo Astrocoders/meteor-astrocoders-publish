ce069d1b0c9d63afe89d2dd27636abeb9eea499a
Gabriel Rubens <grsabreu@gmail.com>
2015-10-25 23:45:53 -0200

    New version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'astrocoders:publish',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Smart re-use your publications.',
   git: 'https://github.com/Astrocoders/meteor-astrocoders-publish',
   documentation: 'README.md'
