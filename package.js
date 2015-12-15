c6507a870e8a0b38f8b9e0dfa030a0995c272c8c
Gabriel Rubens <grsabreu@gmail.com>
2015-12-15 21:30:46 -0200

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'astrocoders:publish',
-  version: '1.0.2',
+  version: '1.0.3',
   summary: 'Smart re-use your publications.',
   git: 'https://github.com/Astrocoders/meteor-astrocoders-publish',
   documentation: 'README.md'
@@ -10,7 +10,6 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
   api.use([
     'ecmascript',
-    'meteor-platform',
     'mongo'
   ]);
 
