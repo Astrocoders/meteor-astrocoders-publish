56ca87970399b251b219b9862440f5cc0b19596c
Gabriel Rubens <grsabreu@gmail.com>
2015-12-26 21:55:39 -0200

    Add method 'ifHasRole'

--- methods.js
+++ methods.js
@@ -1,7 +1,21 @@
 AP.defineMethod({
   type: 'predicate',
   name: 'ifSignedIn',
-  fn: function(){
+  fn(){
     return !!this.userId;
+  }
+});
+
+AP.defineMethod({
+  type: 'predicate',
+  name: 'ifHasRole',
+  context: 'chain',
+  fn(...roles){
+    if(!Package['alanning:roles']){
+      throw new Error('[AstroPublish]',
+        'In order to use `ifHasRole` you need to add alanning:roles');
+    }
+
+    return Package['alanning:roles'].Roles.userIsInRole(this.userId, roles);
   }
 });
