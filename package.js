b9e73d6bf16f5412a961de840de702f352ff6077
Gabriel Rubens <grsabreu@gmail.com>
2015-12-26 21:56:02 -0200

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'astrocoders:publish',
-  version: '1.0.4',
+  version: '1.1.4',
   summary: 'Smart re-use your publications.',
   git: 'https://github.com/Astrocoders/meteor-astrocoders-publish',
   documentation: 'README.md'
